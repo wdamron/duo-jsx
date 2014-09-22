c304d49212d792da86e8e4b95465f4dd55bff53c
emh <evanmarkhopkins@gmail.com>
2014-09-22 12:52:55 -0500

    bump version number

--- test/test.js
+++ test/test.js
@@ -5,13 +5,13 @@ var fs        = require('fs'),
     exp_es6   = fs.readFileSync(__dirname+'/exp_es6.js', 'utf8'),
     test      = require('tape'),
     jsx       = require('../index.js'),
-    duo       = require('duo');
+    Duo       = require('duo');
 
 test('duo-js', function (t) {
     t.plan(3);
     
     //basic usage
-    duo(__dirname+'/tmp/0')
+    Duo(__dirname+'/tmp/0')
      .entry(__dirname+'/react.js')
      .use(jsx())
      .run(function (err, src) {
@@ -21,7 +21,7 @@ test('duo-js', function (t) {
      });
 
     //wrong filetype
-    duo(__dirname+'/tmp/1')
+    Duo(__dirname+'/tmp/1')
      .entry(__dirname+'/react.css')
      .use(jsx())
      .run(function (err, src) {
@@ -29,7 +29,7 @@ test('duo-js', function (t) {
      });
     
     //options
-    duo(__dirname+'/tmp/2')
+    Duo(__dirname+'/tmp/2')
      .entry(__dirname+'/es6.js')
      .use(jsx({harmony: true}))
      .run(function (err, src) {
